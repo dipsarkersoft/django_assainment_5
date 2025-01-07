dc6c1e86bda259cccb391264a22df3b7b0db6bfb
dip_sarker <sarkerdip005@gmail.com>
2025-01-07 22:38:32 +0600

    requirement.txt_added

--- lib_ms/settings.py
+++ lib_ms/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = env("S_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
